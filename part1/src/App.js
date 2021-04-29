b48479afbb2c83c8b3ad22ea1196a998a9ccf3b9
Maxime Ducrocq <maxime.ducrocq03+github@gmail.com>
2021-04-29 16:03:39 +0200

    Part 1 : 1.1 + 1.2 completed

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,5 +1,25 @@
 import React from 'react'
 
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  return <p>{props.part} {props.exercices}</p>
+}
+
+const Content = (props) => {
+  return <>
+    <Part part={props.part1} exercices={props.exercices1} />
+    <Part part={props.part2} exercices={props.exercices2} />
+    <Part part={props.part3} exercices={props.exercices3} />
+  </>
+}
+
+const Total = (props) => {
+  return <p>Number of exercices {props.value}</p>
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -11,17 +31,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercices1}
-      </p>
-      <p>
-        {part2} {exercices2}
-      </p>
-      <p>
-        {part3} {exercices3}
-      </p>
-      <p>Number of exercices {exercices1 + exercices2 + exercices3}</p>
+      <Header course={course} />
+      <Content part1={part1} exercices1={exercices1} part2={part2} exercices2={exercices2} part3={part3} exercices3={exercices3} />
+      <Total value={exercices1 + exercices2 + exercices3} />
     </div>
   )
 }
