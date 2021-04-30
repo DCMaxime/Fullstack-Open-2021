efedf0645e1bd1863d084891be2c05ef2c28d671
Maxime Ducrocq <maxime.ducrocq03+github@gmail.com>
2021-04-30 15:12:11 +0200

    Part 1 : exercise 1.6 completed.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGoodClick = () => setGood(good + 1)
+    const handleNeutralClick = () => setNeutral(neutral + 1)
+    const handleBadClick = () => setBad(bad + 1)
+
+    return (
+        <div>
+            <Header text="Give feedback"/>
+            <Button handleClick={handleGoodClick} text="Good"/>
+            <Button handleClick={handleNeutralClick} text="Neutral"/>
+            <Button handleClick={handleBadClick} text="Bad"/>
+            <Header text="Statistics"/>
+            <Stats text="Good" count={good}/>
+            <Stats text="Neutral" count={neutral}/>
+            <Stats text="Bad" count={bad}/>
+        </div>
+    )
 }
 
-export default App;
+const Header = ({text}) => <h1>{text}</h1>
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+const Stats = ({text, count}) => <p>{text} {count}</p>
+
+export default App
