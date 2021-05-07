b17d60fa8ffd35a54a1dc17d0da84ad1acc87f27
Maxime Ducrocq <maxime.ducrocq03+github@gmail.com>
2021-05-07 16:54:09 +0200

    Part 3 : Exercise 3.5 completed.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -57,6 +57,37 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+const generateId = () => {
+    const min = Math.ceil(5);
+    const max = Math.floor(1000000);
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
