5ea919e8dba8a1dbf00119c76f0994be84d91e01
Maxime Ducrocq <maxime.ducrocq03+github@gmail.com>
2021-05-23 15:39:11 +0200

    Part 5 - Initial commit.

---
+++ part5/bloglist-backend/tests/blog_api.test.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    for (let blog of helper.initialBlogs) {
+        let blogObject = new Blog(blog)
+        await blogObject.save()
+    }
+})
+
+test('there are two blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(2)
+})
+
+test('add another blog to the list', async () => {
+    const initialResponse = await api.get('/api/blogs')
+    const newBlog = {
+        title: "Test de blog 03",
+        author: "Encore Moi-Même",
+        url: "http://git.waryle.com",
+        likes: 36
+    }
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialResponse.body.length + 1)
+    expect(response.body.find((blog) => blog.title === newBlog.title)).toMatchObject(newBlog)
+})
+
+
+afterAll(() => {
+    mongoose.connection.close()
+})
---
+++ part5/bloglist-backend/tests/test_helper.js
@@ -0,0 +1,18 @@
+const initialBlogs = [
+    {
+        title: "Test de blog 01",
+        author: "Moi-Même",
+        url: "http://git.waryle.com",
+        likes: 12
+    },
+    {
+        title: "Test de blog 02",
+        author: "Toujours Moi-Même",
+        url: "http://git.waryle.com",
+        likes: 24
+    }
+]
+
+module.exports = {
+    initialBlogs
+}
