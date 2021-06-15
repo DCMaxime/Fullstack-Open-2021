9c8b303b416c4438c582a87f8d412c24b9de45c2
Maxime Ducrocq <maxime.ducrocq03+github@gmail.com>
2021-06-15 15:11:30 +0200

    Part 6 - Exercice 6.15 completed.

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -3,13 +3,12 @@ import AnecdoteList from './components/AnecdoteList';
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
 import {useDispatch} from "react-redux";
-import anecdoteService from './services/anecdotes'
 import {initializeAnecdotes} from "./reducers/anecdoteReducer";
 
 const App = () => {
     const dispatch = useDispatch()
     useEffect(() => {
-        anecdoteService.getAll().then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
+        dispatch(initializeAnecdotes())
     }, [dispatch])
     return (
         <div>
--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,12 +1,14 @@
-import {createStore, combineReducers} from "redux";
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import thunk from 'redux-thunk'
 import anecdoteReducer, {initializeAnecdotes} from "./reducers/anecdoteReducer";
 import notificationReducer from "./reducers/notificationReducer";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 const reducer = combineReducers({
     anecdotes: anecdoteReducer,
     notification: notificationReducer
 })
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store
