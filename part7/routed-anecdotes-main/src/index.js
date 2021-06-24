b60795d1a2837192847814a85c8e7f70d7169811
Maxime Ducrocq <maxime.ducrocq03+github@gmail.com>
2021-06-24 11:28:18 +0200

    Part 7 : Exercice 7.1 completed.

--- part7/routed-anecdotes-main/src/index.js
+++ part7/routed-anecdotes-main/src/index.js
@@ -1,5 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useRouteMatch,
+    useHistory,
+} from "react-router-dom"
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <Router>
+        <App/>
+    </Router>,
+    document.getElementById('root')
+)
